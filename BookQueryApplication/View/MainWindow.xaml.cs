4a95e0a6c2d48308c465682490522b99be096fbc
Luis Julian <ljulian2190@gmail.com>
2018-10-21 21:59:58 -0700

    Set up a simple UI to hold query results.

--- BookQueryApplication/View/MainWindow.xaml.cs
+++ BookQueryApplication/View/MainWindow.xaml.cs
@@ -16,5 +16,10 @@ namespace BookQueryApplication
             InitializeComponent();
             Closing += (s, e) => ViewModelLocator.Cleanup();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
